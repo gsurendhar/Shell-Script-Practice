0fbfc9546b4cb5f3e5bbd96ac71fa84671bf75e1
Surendhar G <surendharg123@gmail.com>
2025-05-17 08:42:02 +0530

    array file is added

---
+++ 03-array.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+MOVIES=( "mister_perfect" "mirchi" "billa" "munna" )
+echo "first movie   :    ${MOVIES[0]}"
+echo "fourth movie  :    ${MOVIES[3]}"
+echo "second movie  :    ${MOVIES[1]}"
+echo "third movie   :    ${MOVIES[2]}"
+echo "fifth movie   :    ${MOVIES[4]}"
+echo "all movies    :    ${MOVIES[@]}"

29d321f45b435ecdae212bf49b1c291d6ba10d86
Surendhar G <surendharg123@gmail.com>
2025-05-21 19:56:16 +0530

    variable file updated

--- 02-Variables.sh
+++ 02-Variables.sh
@@ -9,8 +9,8 @@ echo "Sum of $NUM1 and $NUM2 is $SUM"
 # sum of two variables, variables passing while executing script in command
 NUM1=$1
 NUM2=$2
-SUM=$(($NUM1+$NUM2))
-echo "Sum of $NUM1 and $NUM2 is $SUM"
+SUM1=$(($NUM1+$NUM2))
+echo "Sum of $NUM1 and $NUM2 is $SUM1"
 
 
 # sum of two variables,  variables passing while executing script(dynamic)
@@ -18,13 +18,13 @@ echo "please enter number1 value"
 read NUM1
 echo "please enter number2 value"
 read NUM2
-SUM=$(($NUM1+$NUM2))
-echo "Sum of $NUM1 and $NUM2 is $SUM"
+SUM2=$(($NUM1+$NUM2))
+echo "Sum of $NUM1 and $NUM2 is $SUM2"
 
 # sum of two variables,  variables passing while executing script(dynamic) with invisible
 echo "please enter number1 value"
 read -s NUM1
 echo "please enter number2 value"
 read -s NUM2
-SUM=$(($NUM1+$NUM2))
-echo "Sum of $NUM1 and $NUM2 is $SUM"
+SUM3=$(($NUM1+$NUM2))
+echo "Sum of $NUM1 and $NUM2 is $SUM3"

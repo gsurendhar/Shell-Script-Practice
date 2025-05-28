6d2c65c5d24cd1109cd32b6ddb6de9c02d174839
Surendhar G <surendharg123@gmail.com>
2025-05-28 22:46:59 +0530

    set file is added

---
+++ set-error-handling.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# set -e 
+echo "hello world"
+echoo "error occurred"
+echo "correct hello script" 
+
+
+set -e # exited when error occurred
+echo "hello world"
+echoo "error occurred"
+echo "correct hello script"

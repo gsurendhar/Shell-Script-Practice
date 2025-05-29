e2031bc42d1ecdf24dae680b2e243dcb59f39b8f
Surendhar G <surendharg123@gmail.com>
2025-05-29 23:00:44 +0530

    backup file is added

---
+++ 11-backup_old_logs.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+SCRIPT_PATH=$PWD
+SOURCE_DIR=$1
+DEST_DIR=$2
+DAYS=${3:-14}
+
+LOG_FOLDER="/var/log/shell-practice"
+SCRIPT_FILE=$( echo $0 | cut -d "." -f1)
+LOG_FILE="$LOG_FOLDER/$SCRIPT_FILE.log"
+
+mkdir -p $LOG_FOLDER
+
+echo "Script started executing at: $(date)" | tee -a $LOG_FILE
+
+# root Privileges checking
+if [ $USERID -eq 0 ]
+then
+    echo -e "$Y you are running with root access $N"   | tee -a $LOG_FILE
+else
+    echo -e " $R ERROR : Please run the Script with root Access $N"  | tee -a $LOG_FILE
+    exit 1
+fi
+
+# to check command line argument passing or not
+if [ $# -lt 2]
+then
+    USAGE
+fi
+
+# USAGE FUNCTION
+USAGE(){
+    echo -e "$R USAGE :: $N sh $0 <SOURCE-DIR> <DESTINATION-DIR> DAYS(Optional)"
+}
+
+#Destination directory existing or not 
+if [ ! -d $DEST_DIR ]
+then 
+    echo -e "$R Destination Directory $DEST_DIR does not exist Please Check $N"
+    exit 1
+fi
+
+#Source directory existing or not 
+if [ ! -d $SOURCE_DIR ]
+then 
+    echo -e "$R Source Directory $SOURCE_DIR does not exist Please Check $N"
+    exit 1
+fi
+
+FILES_TO_DELETE=$(find $SOURCE_DIR -name "*.log" -mtime +$DAYS )  
